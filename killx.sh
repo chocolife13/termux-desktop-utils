ad3643673666b0d5175f7f10f42d6ebccb0e2ffa
Lenny <119404554+chocolife13@users.noreply.github.com>
2026-04-21 12:29:10 +0000

    Add files via upload

---
+++ killx.sh
@@ -0,0 +1,2 @@
+xfce4-session-logout --logout
+sudo /system/bin/am force-stop com.termux.x11
---
+++ startx.sh
@@ -0,0 +1,39 @@
+#!/data/data/com.termux/files/usr/bin/bash
+
+# Kill open X11 processes
+kill -9 $(pgrep -f "termux.x11") 2>/dev/null
+
+
+# Start grphic servers
+# virgl
+#virgl_test_server_android &
+
+# virgl zink
+#MESA_NO_ERROR=1 MESA_GL_VERSION_OVERRIDE=4.3COMPAT MESA_GLES_VERSION_OVERRIDE=3.2 GALLIUM_DRIVER=zink ZINK_DESCRIPTORS=lazy virgl_test_server --use-egl-surfaceless --use-gles &
+
+# zink
+MESA_NO_ERROR=1 MESA_GL_VERSION_OVERRIDE=4.3COMPAT MESA_GLES_VERSION_OVERRIDE=3.2 GALLIUM_DRIVER=zink ZINK_DESCRIPTORS=lazy virgl_test_server --use-egl-surfaceless --use-gles &
+
+
+
+# Enable PulseAudio over Network
+pulseaudio --start --load="module-native-protocol-tcp auth-ip-acl=127.0.0.1 auth-anonymous=1" --exit-idle-time=-1
+
+# Prepare termux-x11 session
+export XDG_RUNTIME_DIR=${TMPDIR}
+termux-x11 :0 >/dev/null &
+
+# Wait a bit until termux-x11 gets started.
+sleep 3
+
+# Launch Termux X11 main activity
+am start --user 0 -n com.termux.x11/com.termux.x11.MainActivity > /dev/null 2>&1
+sleep 1
+
+# Set audio server
+export PULSE_SERVER=127.0.0.1
+
+# Run XFCE4 Desktop
+env DISPLAY=:0 dbus-launch --exit-with-session xfce4-session & > /dev/null 2>&1
+
+exit 0
